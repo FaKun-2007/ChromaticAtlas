8701c7c06107ca2d3ee83f5e2036fd35773d48df
Esanov Nazar <118618851+FaKun-2007@users.noreply.github.com>
2023-12-15 20:59:39 +0200

    telegram.php

--- telegram.php
+++ telegram.php
@@ -1,11 +1,30 @@
 <?php
 
 /* https://api.telegram.org/botXXXXXXXXXXXXXXXXXXXXXXX/getUpdates,
-где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
+где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее  	
+<div class="form-group">
+      				<label for="">Імя</label>
+      				<input type="text" class="form-control" id="" name="user_name" placeholder="Наприклад, Іван">
+      			</div>
+      		
+      			<div class="form-group">
+      				<label for="">Номер</label>
+      				<input type="text" class="form-control" id="" name="user_phone" placeholder="Наприклад, 0974743503">
+      			</div>
+      		
+      			<div class="form-group">
+      				<label for="">Соц.мережа</label>
+      				<input type="text" class="form-control" id="" name="user_email" placeholder="Інстаграм або Телеграм">
+      			</div>
+			<div class="form-group">
+      				<label for="">Товар</label>
+      				<input type="text" class="form-control" id="" name="user_picture" placeholder="Наприклад, Кошечки хорошие">
+      			</div>*/
 
 $name = $_POST['user_name'];
 $phone = $_POST['user_phone'];
 $email = $_POST['user_email'];
+$picture = $_POST['user_picture'];
 $token = "";
 $chat_id = "";
 $arr = array(
